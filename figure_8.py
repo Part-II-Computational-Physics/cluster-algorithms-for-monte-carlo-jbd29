543561f86a054553848433728951596d29cd6a4a
Jack Dixon <jbd29@cam.ac.uk>
2023-04-20 10:43:55 +0100

    \

--- figure_8.py
+++ figure_8.py
@@ -13,7 +13,7 @@ importlib.reload(acf)
 # Produces a plot of average autocorrelation time against temperature for MH and Wolff 
 
 # Initialise required variables for MH tau against T:
-Ts = np.linspace(0.1,3.5,15)
+Ts = np.linspace(0.2,3.5,15)
 max_time = 50000
 autocorr_times_MH = []
 autocorr_times_err_MH = []
@@ -22,7 +22,7 @@ for T in Ts:
 
     for i in range(5):
         # Lattice must be reset in each iteration, and evolved to (beyond) equilibrium
-        lattice = lat.make_lattice(75,1)
+        lattice = lat.make_lattice(60,1)
         burn = MH.evolve_and_compute_M(lattice, T**-1, 1, 0, max_time)[0]
 
         # evolve the lattice from equilibrium
@@ -33,6 +33,7 @@ for T in Ts:
         autocorr = acf.compute_autocorrelation(Ms)
         sweeps_tau_f = acf.estimate_correlation_time(autocorr)/np.size(lattice)
         autocorr_times_i.append(sweeps_tau_f)
+        print(i)
 
     # Take sample mean and error
     autocorr_times_MH.append(np.mean(autocorr_times_i))
@@ -44,7 +45,7 @@ for T in Ts:
 #np.save('MH_autocorrelation_against_T_err', autocorr_times_err)
 
 # Reset required variables for Wolff tau against T, smaller max time needed due to smaller autocorrelation time for Wolff:
-Ts = np.linspace(0.1,3.5,15)
+Ts = np.linspace(0.2,3.5,15)
 max_time = 1000
 autocorr_times_Wolff = []
 autocorr_times_err_Wolff = []
@@ -53,7 +54,7 @@ for T in Ts:
 
     for i in range(5):
         # Lattice must be reset after each iteration and evolved to (beyond) equilibrium
-        lattice = lat.make_lattice(75,1)
+        lattice = lat.make_lattice(60,1)
         burn = W.Wolff_evolve_and_compute_M(lattice, T**-1, 1, max_time)
 
         # evolve the lattice from equilibrium
@@ -64,6 +65,7 @@ for T in Ts:
         autocorr = acf.compute_autocorrelation(Ms)
         sweeps_tau_f = sweeps[acf.estimate_correlation_time(autocorr)]
         autocorr_times_i.append(sweeps_tau_f)
+        print(i)
     
     # Take sample mean and error
     autocorr_times_Wolff.append(np.mean(autocorr_times_i))

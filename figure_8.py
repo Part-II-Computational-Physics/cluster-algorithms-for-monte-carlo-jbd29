5e002a9c095a64ba2d825714d1fa65c5471457b7
Jack Dixon <jbd29@cam.ac.uk>
2023-04-21 17:36:36 +0100

    energy thermalisation

--- figure_8.py
+++ figure_8.py
@@ -10,27 +10,30 @@ importlib.reload(W)
 importlib.reload(lat)
 importlib.reload(acf)
 
-#Produces the data for figure 
+#Produces the data for figure 8
 
 # initialise lattice
 lattice = lat.make_lattice(25,0)
 # evolve to eq with MH
-MH_Ms, MH_sweeps = MH.evolve_and_compute_M(lattice, 1**-1, 1, 0, 100000)
+MH_Es, MH_sweeps = MH.evolve_and_compute_E(lattice, 1**-1, 1, 0, 100000)
+print('initial MH done')
 # evolve after eq with MH
-MH_thermal_Ms, MH_thermal_sweeps = MH.evolve_and_compute_M(lattice, 1**-1, 1, 0, 50000)
+MH_thermal_Es, MH_thermal_sweeps = MH.evolve_and_compute_E(lattice, 1**-1, 1, 0, 50000)
+print('thermalised MH done')
 # reset lattice
 lattice = lat.make_lattice(25,0)
 # evolve to eq with Wolff
-Wolff_Ms, Wolff_sweeps = W.Wolff_evolve_and_compute_M(lattice, 1**-1, 1, 100)
+Wolff_Es, Wolff_sweeps = W.Wolff_evolve_and_compute_E(lattice, 1**-1, 1, 100)
+print('intitial Wolff done')
 # evolve after eq with Wolff
-Wolff_thermal_Ms, Wolff_thermal_sweeps = W.Wolff_evolve_and_compute_M(lattice, 1**-1, 1, 50)
+Wolff_thermal_Es, Wolff_thermal_sweeps = W.Wolff_evolve_and_compute_E(lattice, 1**-1, 1, 50)
 
 # Save
-np.save('MH_thermalisation_Ms', MH_Ms)
-np.save('MH_thermalisation_sweeps', MH_sweeps)
-np.save('Wolff_thermalisation_Ms', Wolff_Ms)
-np.save('Wolff_thermalisation_sweeps', Wolff_sweeps)
-np.save('MH_thermalised_Ms', MH_thermal_Ms)
-np.save('MH_thermalised_sweeps', MH_thermal_sweeps)
-np.save('Wolff_thermalised_Ms', Wolff_thermal_Ms)
-np.save('Wolff_thermalised_sweeps', Wolff_thermal_sweeps)
+np.save('MH_thermalisation_Es', MH_Es)
+np.save('MH_thermalisation_sweeps_Es', MH_sweeps)
+np.save('Wolff_thermalisation_Es', Wolff_Es)
+np.save('Wolff_thermalisation_sweeps_Es', Wolff_sweeps)
+np.save('MH_thermalised_Es', MH_thermal_Es)
+np.save('MH_thermalised_sweeps_Es', MH_thermal_sweeps)
+np.save('Wolff_thermalised_Es', Wolff_thermal_Es)
+np.save('Wolff_thermalised_sweeps_Es', Wolff_thermal_sweeps)

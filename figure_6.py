2d531403a9cf825208bce4156e65aefd69f32138
Jack Dixon <jbd29@cam.ac.uk>
2023-04-21 16:54:37 +0100

    data for figure 6

--- figure_6.py
+++ figure_6.py
@@ -16,7 +16,21 @@ importlib.reload(acf)
 lattice = lat.make_lattice(25,0)
 # evolve to eq with MH
 MH_Ms, MH_sweeps = MH.evolve_and_compute_M(lattice, 1**-1, 1, 0, 100000)
+# evolve after eq with MH
 MH_thermal_Ms, MH_thermal_sweeps = MH.evolve_and_compute_M(lattice, 1**-1, 1, 0, 50000)
+# reset lattice
 lattice = lat.make_lattice(25,0)
+# evolve to eq with Wolff
 Wolff_Ms, Wolff_sweeps = W.Wolff_evolve_and_compute_M(lattice, 1**-1, 1, 100)
+# evolve after eq with Wolff
 Wolff_thermal_Ms, Wolff_thermal_sweeps = W.Wolff_evolve_and_compute_M(lattice, 1**-1, 1, 50)
+
+# Save
+np.save('MH_thermalisation_Ms', MH_Ms)
+np.save('MH_thermalisation_sweeps', MH_sweeps)
+np.save('Wolff_thermalisation_Ms', Wolff_Ms)
+np.save('Wolff_thermalisation_sweeps', Wolff_sweeps)
+np.save('MH_thermalised_Ms', MH_thermal_Ms)
+np.save('MH_thermalised_sweeps', MH_thermal_sweeps)
+np.save('Wolff_thermalised_Ms', Wolff_thermal_Ms)
+np.save('Wolff_thermalised_sweeps', Wolff_thermal_sweeps)

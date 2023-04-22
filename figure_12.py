48a904b41df2e510bbbdd0204b0b84b778904aa1
Jack Dixon <jbd29@cam.ac.uk>
2023-04-22 16:34:37 +0100

    corrections

--- figure_12.py
+++ figure_12.py
@@ -42,7 +42,7 @@ for i in range(5):
 
 # Take Averages
 MH_autocorr = np.mean(MH_autocorr_temp, axis = 0)
-MH_sweeps_tau_f = acf.estimate_correlation_time(MH_autocorr)
+MH_sweeps_tau_f = sweeps_MH[acf.estimate_correlation_time(MH_autocorr)]
 Wolff_autocorr = np.mean(Wolff_autocorr_temp, axis = 0)
 Wolff_sweeps_tau_f =acf.estimate_correlation_time(Wolff_autocorr)
 

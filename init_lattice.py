b9ae8b7da19eaaa7924de3c44744998c7185aebe
Jack Dixon <jbd29@cam.ac.uk>
2023-04-22 21:14:13 +0100

    a

--- init_lattice.py
+++ init_lattice.py
@@ -40,12 +40,13 @@ def compute_magnetisation(lattice):
     M = np.abs(np.sum(lattice)/np.size(lattice))
     return M
 
-def compute_Chi(lattice, beta, M_Sample):
+def compute_Chi(lattice, T, M_Sample):
     # takes in a list of magnetisations from some sampling scheme
     # and returns susceptibility 
-    return beta*np.size(lattice)*np.var(M_Sample)
+    return (T**-1)*np.size(lattice)*np.var(M_Sample)
 
 def compute_E(lattice, J):
+    # Computes the internal energy of the lattice
     width = len(lattice)
     E = 0
     for j in range(len(lattice)):
@@ -53,35 +54,66 @@ def compute_E(lattice, J):
             E += -0.5*J*lattice[i][j]*neighbouring_spins_sum(i,j, lattice, width)
     return  E
 
+# Blocking method to determine system observables according to scheme in report
 def batch_average(observable):
     # Takes a thermalised variable and batches the data
     tau_f = acf.estimate_correlation_time(acf.compute_autocorrelation(observable))
     print('tau_f = ' + str(tau_f))
 
     if tau_f == 0:
+        # Failsafe for 0 tau
         n_batches = int(np.floor(len(observable)/2))
         batch_length = 2
     else:
+        # Divide into batches of 2 tau
         n_batches = int(np.floor(len(observable)/(2*tau_f)))
         batch_length = 2*tau_f
 
     print('Data divided into ' + str(n_batches) + ' batches.')
-    
     average = []
     average_square = []
-
+    
+    # Create batches of at least 2 tau elements
     for i in range(n_batches):
         if i == n_batches - 1:
             batch = observable[i*batch_length:]
         else:
             batch = observable[i*batch_length:(i+1)*batch_length] 
-        
+
+        # Temporary storage of batch measures
         average.append(np.mean(batch))
         average_square.append(np.mean(np.power(batch,2)))
 
+    # Final calculation of averaage and error as described in report
     average = np.mean(average)
     average_square = np.mean(average_square)
     err = np.sqrt(np.abs(((n_batches - 1)**-1)*(average_square - average**2)))
     return average, err
+
+def batch_data(observable):
+    # Batches data without averaging to allow computation of derived observables
+    tau_f = acf.estimate_correlation_time(acf.compute_autocorrelation(observable))
+    print('tau_f = ' + str(tau_f))
+
+    if tau_f == 0:
+        # Failsafe for 0 tau
+        n_batches = int(np.floor(len(observable)/2))
+        batch_length = 2
+    else:
+        # Divide into batches of 2 tau
+        n_batches = int(np.floor(len(observable)/(2*tau_f)))
+        batch_length = 2*tau_f
+
+    print('Data divided into ' + str(n_batches) + ' batches.')
+    batches = []
+
+    # Create batches of at least 2 tau elements
+    for i in range(n_batches):
+        if i == n_batches - 1:
+            batches.append(observable[i*batch_length:])
+        else:
+            batches.append(observable[i*batch_length:(i+1)*batch_length])
+
+    return batches
 
 

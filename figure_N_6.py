9238ab525f8357e3e9416eda03fef6a5c0051900
Jack Dixon <jbd29@cam.ac.uk>
2023-04-22 21:46:21 +0100

    a

--- figure_N_6.py
+++ figure_N_6.py
@@ -20,20 +20,27 @@ Ts_MH = np.linspace(1,5,30)
 Ts_Wolff = np.linspace(1,5,30)
 
 # Initialise needed averaging times and observable array:
-Ms_MH = []
-Ms_MH_err = []
-Ms_Wolff = []
-Ms_Wolff_err = []
+Chis_MH = []
+Chis_MH_err = []
+Chis_Wolff = []
+Chis_Wolff_err = []
 
 # Evolve and loop through Ts:
 for T in Ts_MH:
-    burn = MH.evolve_and_compute_M(lattice_MH,T**-1, 1, 0, 500000)[0]
-    Ms = MH.evolve_and_compute_M(lattice_MH,T**-1,1,0,100000)[0]
+    burn = MH.evolve_and_compute_M(lattice_MH, T**-1, 1, 0, 100000)[0]
+    Ms = MH.evolve_and_compute_M(lattice_MH, T**-1, 1, 0, 100000)[0]
+    
+    #Find Chi for T using blocking:
+    Chis_T = []
+    Ms_samples = lat.batch_data(Ms)
+    for i in Ms_samples:
+        Chis_T.append(lat.compute_Chi(i,))
 
-    Ms_MH_T, Ms_MH_err_T = lat.batch_average(Ms)
-    print(Ms_MH_T)
-    Ms_MH.append(Ms_MH_T)
-    Ms_MH_err.append(Ms_MH_err_T)
+    # Take average and error:
+    Chis_MH.append(np.mean(Chis_T))
+
+
+
 
 
 print('MH Done')

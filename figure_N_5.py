adb8024ce15e42c391e589a384286c0974d81668
Jack Dixon <jbd29@cam.ac.uk>
2023-04-23 14:39:18 +0100

    corrections

--- figure_N_5.py
+++ figure_N_5.py
@@ -3,13 +3,14 @@ import init_lattice as lat
 import MH_algorithm as MH
 import Wolff_algorithm as W
 import autocorrelation_functions as acf
+import statsmodels.api as sm
 import importlib
 importlib.reload(MH)
 importlib.reload(W)
 importlib.reload(lat)
 importlib.reload(acf)
 
-# Program for use in figure ten to determine the finite scaling law for the lattice
+# Program for use in report to determine the finite scaling law for the lattice
 
 # First initialise lattice widths
 lattice_widths = [2,5,10,50,100]
@@ -20,55 +21,77 @@ T_c_Wolff = np.load('T_c_Wolff.npy')
 
 #Run loop for MH algorithm
 MH_autocorr_time_against_width = []
-MH_autocorr_time_against_width_err = []
 
 for width in lattice_widths:
-# Loop for averaging
+
+    # Temp data storage
+    MH_autocorr = []
+    # Loop for averaging
     for i in range(5):
-        # Create temporary lattice for sample measurements
-        MH_temp = []
 
         # Initialise lattice
         lattice = lat.make_lattice(width,1)
         # Burn to equilibrium 
         burn = MH.evolve_and_compute_M(lattice, T_c_MH**-1, 1, 0, 50000)[0]
         # Evolve and find autocorrelation time
-        Ms, sweeps = MH.evolve_and_compute_M(lattice,T_c_MH**-1,1,0,50000)
+        Ms = MH.evolve_and_compute_M(lattice, T_c_MH**-1, 1, 0, 50000)[0]
+
         # Batch up the data to get more measurements:
         for j in range(5):
             Ms_sample = np.array_split(Ms, 5)[j]
-            MH_autocorr = acf.compute_autocorrelation(Ms)
-            MH_temp.append(acf.estimate_correlation_time(MH_autocorr))
-    print(i)
+            MH_autocorr.append(acf.compute_autocorrelation(Ms_sample))
 
-    MH_autocorr_time_against_width.append(np.mean(MH_temp))
-    MH_autocorr_time_against_width_err.append(np.std(MH_temp))
+    print(i)
+    MH_autocorr_time_against_width.append(acf.estimate_correlation_time(np.mean(MH_autocorr, axis = 0)))
     print('Width = ' + str(width) + ', autocorr = ' + str(MH_autocorr_time_against_width))
 
 # Repeat whole process above for the Wolff algorithm
 Wolff_autocorr_time_against_width = []
-Wolff_autocorr_time_against_width_err = []
 
 for width in lattice_widths:
+
+    # Temp data storage
+    Wolff_autocorr = []
     for i in range(5):
-        Wolff_temp = []
 
-        lattice = lat.make_lattice(width,1)
-        burn = W.Wolff_evolve_and_compute_M(lattice,T_c_Wolff**-1,1,1000)[0]
-        Ms, sweeps = W.Wolff_evolve_and_compute_M(lattice,T_c_Wolff**-1,1,2500)
+        lattice = lat.make_lattice(width, 1)
+        burn = W.Wolff_evolve_and_compute_M(lattice, T_c_Wolff**-1, 1, 1000)[0]
+        Ms = W.Wolff_evolve_and_compute_M(lattice, T_c_Wolff**-1, 1, 2000)[0]
 
-        for j in range(6):
-            Ms_sample = np.array_split(Ms, 6)[j]
-            Wolff_autocorr = acf.compute_autocorrelation(Ms)
-            Wolff_temp.append(acf.estimate_correlation_time(Ms))
+        for j in range(5):
+            Ms_sample = np.array_split(Ms, 5)[j]
+            Wolff_autocorr.append(acf.compute_autocorrelation(Ms_sample))
+
     print(i)
-
-    Wolff_autocorr_time_against_width.append(np.mean(Wolff_temp))
-    Wolff_autocorr_time_against_width_err.append(np.std(Wolff_temp))
+    Wolff_autocorr_time_against_width.append(acf.compute_autocorrelation(np.mean(Wolff_autocorr, axis = 0)))
     print('Width = ' + str(width) + ', Wolff autocorr = ' + str(Wolff_autocorr_time_against_width))
 
 np.save('MH_autocorr_time_against_width.npy', MH_autocorr_time_against_width)
-np.save('Wolff_autocorr_time_against_width.npy', Wolff_autocorr_time_against_width)
-np.save('MH_autocorr_time_against_width_err.npy', MH_autocorr_time_against_width_err)
-np.save('Wolff_autocorr_time_against_width_err.npy', Wolff_autocorr_time_against_width_err)
 
+# Now use statsmodels to fit the data to linear regression:
+# Firstly take logs:
+lnL = np.log(lattice_widths)
+lnMH_autocorr_time_against_width = np.log(MH_autocorr_time_against_width)
+lnWolff_autocorr_time_against_width = np.log(Wolff_autocorr_time_against_width)
+
+# Linear regression for MH
+A = np.vstack([lnL, np.ones(len(lnMH_autocorr_time_against_width))]).T
+model = sm.OLS(lnMH_autocorr_time_against_width, A)
+results = model.fit()
+m , c = results.params
+m_err, c_err = results.bse
+
+# Save for use in report
+np.save('MH_autocorr_time_against_width_params.npy', [m, c])
+np.save('MH_autocorr_time_against_width_params_err.npy', [m_err, c_err])
+
+# Linear regression for Wolff
+A = np.vstack([lnL, np.ones(len(lnWolff_autocorr_time_against_width))]).T
+model = sm.OLS(lnWolff_autocorr_time_against_width, A)
+results = model.fit()
+m , c = results.params
+m_err, c_err = results.bse
+
+# Save for use in report 
+np.save('MH_autocorr_time_against_width_params.npy', [m, c])
+np.save('MH_autocorr_time_against_width_params_err.npy', [m_err, c_err])
